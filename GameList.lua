2b4304c3e9de1304206492016e7845be21af9d57
HonestlyPizza <112041619+HonestlyPizza@users.noreply.github.com>
2025-03-30 21:23:52 -0400

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [2753915549] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7998cf242a0c763875a90c5300d2e937.lua"))()", -- Blox Fruit Sea 1
+  [2753915549] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/88e15b64a18c1a4fdb83120288e748be.lua"))()", -- Blox Fruit Sea 1
   [4442272183] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7998cf242a0c763875a90c5300d2e937.lua"))()", -- Blox Fruit Sea 2
   [7449423635] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7998cf242a0c763875a90c5300d2e937.lua"))()", -- Blox Fruit Sea 3
   [10449761463] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7998cf242a0c763875a90c5300d2e937.lua"))()",  -- The Strongest Battlegrounds
