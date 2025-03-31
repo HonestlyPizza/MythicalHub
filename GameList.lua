dfd224d227ebaf49b6fafc2fff4ad865de0df179
HonestlyPizza <112041619+HonestlyPizza@users.noreply.github.com>
2025-03-30 21:03:29 -0400

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,12 @@
+local Games = {
+  [2753915549] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7ce2ee79eeb7180ec99d44750e61de0c.lua"))()", -- Blox Fruit Sea 1
+  [4442272183] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7ce2ee79eeb7180ec99d44750e61de0c.lua"))()", -- Blox Fruit Sea 2
+  [7449423635] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7ce2ee79eeb7180ec99d44750e61de0c.lua"))()", -- Blox Fruit Sea 3
+  [10449761463] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua",  -- The Strongest Battlegrounds
+  [16732694052] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/88e15b64a18c1a4fdb83120288e748be.lua"))()", -- Fisch
+  [72907489978215] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/88e15b64a18c1a4fdb83120288e748be.lua"))()", -- 2 Sea Fisch
+  [116495829188952] = "add", -- Dead Rails
+  [70876832253163] = "add", -- Dead Rails
+}
+
+return Games
